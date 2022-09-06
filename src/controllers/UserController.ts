9241aa7ed68a46e7c7b319ebeb045d7a17823f8a
amandahammes <82393453+amandahammes@users.noreply.github.com>
2022-09-05 22:54:44 -0300

    crud user e post

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,7 +1,9 @@
+import { userRepository } from './../repositories/userRepository';
+import { User } from './../entity/User';
 import { Request, Response } from "express"
-import { userRepository } from "../repositories/userRepository"
 
 export class UserController {
+    
 	static create = async (req: Request, res: Response) => {
 		const { name, email, apartment, password  } = req.body
 
@@ -15,7 +17,66 @@ export class UserController {
 			console.log(error)
 			return res.status(500).json({ message: 'Internal Server Error' })
 		}
-
-        return res.status(201).send('Fooooi!')
 	}
+
+	static edit = async (req: Request, res: Response) => {
+		const id: number = parseInt(req.params.iduser, 10)
+		const { name, email, apartment } = req.body
+		let user: User
+
+		try {
+			user = await userRepository.findOneByOrFail({id})
+		} catch (error) {
+			return res.status(404).send("User not found")
+		}
+
+		if(name) {
+			user.name = name
+		}
+		
+		if(email) {
+			user.email = email
+		}
+
+		if(apartment) {
+			user.apartment = apartment
+		}
+	}	
+
+	static listId = async (req: Request, res: Response) => {
+        const id: number = parseInt(req.params.iduser, 10)
+        let user: User
+
+        try {
+            user = await userRepository.findOneByOrFail({id})
+        } catch (error) {
+            return res.status(404).send("User not found")            
+        }
+       
+        return res.send(user)
+    }
+
+	static listAll = async (req: Request, res: Response) => {      
+        const users = await userRepository.find({
+            select: ["name", "email", "apartment" ]
+        })
+
+        return res.send(users)
+    }	
 }
+
+
+// static del = async (req:Request, res: Response) => {
+	// 	const id: any = req.params.id
+	// 	let user: User
+
+	// 	try {
+	// 		user = await userRepository.findOneOrFail(id)
+	// 	} catch (error) {
+	// 		return res.status(404).send("User not found.")
+	// 	}
+		
+	// 	userRepository.delete(id)
+
+	// 	return res.status(204).send()
+	// }
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -6,12 +6,12 @@ const routes = Router()
 //create new user
 routes.post("/", UserController.create)
 //editar usuário
-
+routes.put("/:id", UserController.edit)
 //deletar usuário
 
 //listar por id
-
+routes.get("/:id", UserController.listId)
 //listar todos
-
+routes.get("/", UserController.listAll)
 
 export default routes
